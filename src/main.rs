2cc4600b695fbc5c429317bb5d1147ba990fc35b
shizukani-cp <to.maimai0606@gmail.com>
2024-04-07 14:31:54 +0900

    READMEを作成 arraの方で仕様変更をしたので編集 空行無視の部分を追加

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,25 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn generate_code(filename:&String) -> Vec<Vec<String>> {
-    // --snip--
-    //println!("In file {}", filename);
-
-    // ファイルが見つかりませんでした
     let mut f = File::open(filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
-        // ファイルの読み込み中に問題がありました
         .expect("something went wrong reading the file");
     let str_contents = contents.as_str();
     let mut code = vec![];
     for str_row in str_contents.split("\n"){
         let mut code_row = vec![];
         for cell in str_row.split(","){
-            code_row.push(cell.trim().to_string());
+            let cell_code = cell.trim().to_string();
+            if cell_code != ""{
+                code_row.push(cell_code);
+            }
         }
-        code.push(code_row);
+        if !(code_row.is_empty()){
+            code.push(code_row);
+        }
     }
-    // テキストは\n{}です
-    //println!("With text:\n{}", contents);
     return code;
 }
 
