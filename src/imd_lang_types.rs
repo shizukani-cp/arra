db0766c0646cadea5e352a01d33fd0168f1385dd
shizukani-cp <to.maimai0606@gmail.com>
2024-05-03 14:58:09 +0900

    ImdLangTypesを別ファイルに移管

---
+++ src/imd_lang_types.rs
@@ -0,0 +1,84 @@
+pub mod ImdLangTypes {
+
+    pub type Statements = Vec<Statement>;
+
+    #[derive(Debug)]
+    pub enum Symbols {
+        Add,
+        Sub,
+        Mul,
+        Div,
+        Mod,
+        Pow,
+        Equal
+    }
+
+    #[derive(Debug)]
+    pub enum SymbolAndValues {
+        Lit(Literal),
+        Variable(Var),
+        Simbol(Symbols)
+    }
+
+    #[derive(Debug)]
+    pub struct Expression {
+        symbol_and_values:Vec<SymbolAndValues>
+    }
+
+    impl Expression {
+        fn to_literal(&self) -> Literal {
+            panic!("this method is not implemented.");
+            Literal {
+                type_:"Error".to_string(),
+                value:"Error".to_string()
+            }
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct Literal {
+        type_:String,
+        value:String
+    }
+
+    #[derive(Debug)]
+    pub enum HasLiteralAndEmpty {
+        Empty,
+        Lit(Literal)
+    }
+
+    #[derive(Debug)]
+    pub struct Var {
+        varname:String,
+        value:HasLiteralAndEmpty
+    }
+
+    #[derive(Debug)]
+    pub struct Case {
+        condition:Expression,
+        block:Box<Statements>
+    }
+
+    #[derive(Debug)]
+    pub enum Statement {
+        Call{
+            func:Expression,
+            args:Vec<Expression>
+        },
+        Switch {
+            cases:Vec<Case>
+        },
+        Loop {
+            block:Box<Statements>
+        },
+        Substitution {
+            var:Var,
+            right_hand_side:Expression
+        },
+        Instance {
+            type_:String,
+            args:Vec<Expression>
+        },
+        Tmp
+    }
+}
