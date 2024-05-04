62e46d65d90b551cc60486060ddd0d65223486b1
shizukani-cp <to.maimai0606@gmail.com>
2024-05-04 15:04:11 +0900

    中間言語にする部分を別ファイルに移動

---
+++ src/compile.rs
@@ -0,0 +1,79 @@
+pub mod compiler{
+    use std::fs::File;
+    use std::io::prelude::*;
+
+    use crate::imd_lang_types::ImdLangTypes;
+    
+    pub fn generate_code(filename:&String) -> ImdLangTypes::Statements {
+        let vec_code = str_code_to_vec(read_file(filename));
+        println!("{:?}", vec_code);
+        vec_code_to_imd_lang(vec_code)
+    }
+
+    pub fn read_file(filename:&String) -> String{
+        let mut f = File::open(filename)
+            .expect("file not found");
+        let mut contents = String::new();
+        f.read_to_string(&mut contents)
+            .expect("something went wrong reading the file");
+        contents
+    }
+
+    pub fn str_code_to_vec(str_code:String) -> Vec<Vec<String>> {
+        let mut code = vec![];
+        for str_row in str_code.as_str().split("\n"){
+            let mut code_row = vec![];
+            let sp: Vec<_> = str_row.split(",").collect();
+            let mut cells = sp.iter().peekable();
+            loop{
+                let mut cell_code = "".to_string();
+                let mut in_str = false;
+                loop{
+                    let code = match cells.next(){
+                        Some(v) => v,
+                        _ => ""
+                    }.replace("\r", "\n");
+                    if !in_str{
+                        cell_code += code.trim();
+                    } else {
+                        cell_code += &(",".to_string().to_owned() + &code.lines().collect::<String>());
+                    }
+                    if cell_code.chars().next() == None {
+                        break;
+                    }
+                    //println!("{}", cell_code.chars().next().unwrap());
+                    //println!("{:?}", cell_code.chars());
+                    if cell_code.chars().next().unwrap() == '#' {
+                        cell_code = "".to_string();
+                    }
+                    let mut back_slash_num = 0;
+                    for s in cell_code.chars() {
+                        if s.to_string() == "\"".to_string(){
+                            back_slash_num += 1;
+                        }
+                    }
+                    if back_slash_num % 2 == 0 {
+                        break;
+                    } else {
+                        in_str = true;
+                    }
+                }
+                if cell_code != ""{
+                    code_row.push(cell_code);
+                }
+                if cells.peek() == None {
+                    break;
+                }
+            }
+            if !(code_row.is_empty()){
+                code.push(code_row);
+            }
+        }
+        code
+    }
+
+    pub fn vec_code_to_imd_lang(veccode:Vec<Vec<String>>) -> ImdLangTypes::Statements {
+        //panic!("this method is not implemented.");
+        vec![ImdLangTypes::Statement::Tmp]
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,19 @@
-use std::env;
-use std::fs::File;
-use std::io::prelude::*;
-
 mod imd_lang_types;
 use imd_lang_types::ImdLangTypes;
 
-fn generate_code(filename:&String) -> ImdLangTypes::Statements {
-    let vec_code = str_code_to_vec(read_file(filename));
-    println!("{:?}", vec_code);
-    vec_code_to_imd_lang(vec_code)
-}
+mod compile;
+use compile::compiler;
 
-fn read_file(filename:&String) -> String{
-    let mut f = File::open(filename)
-        .expect("file not found");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-    contents
-}
-
-fn str_code_to_vec(str_code:String) -> Vec<Vec<String>> {
-    let mut code = vec![];
-    for str_row in str_code.as_str().split("\n"){
-        let mut code_row = vec![];
-        let sp: Vec<_> = str_row.split(",").collect();
-        let mut cells = sp.iter().peekable();
-        loop{
-            let mut cell_code = "".to_string();
-            let mut in_str = false;
-            loop{
-                let code = match cells.next(){
-                    Some(v) => v,
-                    _ => ""
-                }.replace("\r", "\n");
-                if !in_str{
-                    cell_code += code.trim();
-                } else {
-                    cell_code += &(",".to_string().to_owned() + &code.lines().collect::<String>());
-                }
-                if cell_code.chars().next() == None {
-                    break;
-                }
-                //println!("{}", cell_code.chars().next().unwrap());
-                //println!("{:?}", cell_code.chars());
-                if cell_code.chars().next().unwrap() == '#' {
-                    cell_code = "".to_string();
-                }
-                let mut back_slash_num = 0;
-                for s in cell_code.chars() {
-                    if s.to_string() == "\"".to_string(){
-                        back_slash_num += 1;
-                    }
-                }
-                if back_slash_num % 2 == 0 {
-                    break;
-                } else {
-                    in_str = true;
-                }
-            }
-            if cell_code != ""{
-                code_row.push(cell_code);
-            }
-            if cells.peek() == None {
-                break;
-            }
-        }
-        if !(code_row.is_empty()){
-            code.push(code_row);
-        }
-    }
-    code
-}
-
-fn vec_code_to_imd_lang(veccode:Vec<Vec<String>>) -> ImdLangTypes::Statements {
-    //panic!("this method is not implemented.");
-    vec![ImdLangTypes::Statement::Tmp]
-}
+use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
-    println!("{:?}", generate_code(&filename));
+    if args[1] == "compile".to_string(){
+        let filename = &args[2];
+        println!("{:?}", compiler::generate_code(&filename));
+    } else if args[1] == "run".to_string(){
+
+    } else {
+        panic!("Sub command not found.");
+    }
 }
